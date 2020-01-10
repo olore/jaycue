4bafe788b965af4618ac2e1f1aa847191b4d2f04
Brian Olore <brian@olore.net>
2020-01-10 08:59:40 -0500

    test: add more tests from examples

--- jqx.js
+++ jqx.js
@@ -5,9 +5,15 @@ const jqx = (jsonObject, query) => {
     return jsonObject;
   }
 
+  let result = undefined;
   if (query.startsWith('.')) {
-    let x = get(jsonObject, query.substring(1));
-    return x;
+    if (/\?$/.test(query)) {
+      query = query.substring(1, query.length-1);
+      result = get(jsonObject, query);
+    } else {
+      result = get(jsonObject, query.substring(1));
+    }
+    return result;
   }
 }
 
--- tests/first.test.js
+++ tests/first.test.js
@@ -9,12 +9,34 @@ test('Identity', () => {
 });
 
 
-test('Object Identifier-Index', () => {
+test('Object Identifier-Index 1', () => {
   let query = '.foo';
   let json = {"foo": 42, "bar": "less interesting data"}
   expect(jqx(json, query)).toEqual(jq(json, query));
 });
 
+test('Object Identifier-Index 2', () => {
+  let query = '.foo';
+  let json = {"notfoo": true, "alsonotfoo": false};
+
+  // when jq returns null, we return undefined as it's more idiomatic in JS
+  expect(jqx(json, query)).toEqual(undefined);
+  expect(jq(json, query)).toEqual("null");
+});
+
+test('Object Identifier-Index 3', () => {
+  let query = '.["foo"]';
+  let json = {"foo": 42};
+
+  expect(jqx(json, query)).toEqual(jq(json, query));
+});
+
+test('Optional Object Identifier-Index', () => {
+  let query = '.foo?';
+  let json = {"foo": 42, "bar": "less interesting data"};
+
+  expect(jqx(json, query)).toEqual(jq(json, query));
+});
 
 test('can get top level string', () => {
   let query = '.versionString';
@@ -57,7 +79,7 @@ const echoify = (jsonObject) => {
 
 // run actual jq command 
 function jq(jsonObject, query) {
-  let command = `echo "${echoify(jsonObject)}" | jq ${query}`;
+  let command = `echo "${echoify(jsonObject)}" | jq '${query}'`;
 
   let result = exec(command).toString();
   
