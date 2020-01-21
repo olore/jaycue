ef88ae06dc4fb22a4e23cabe3b0b31d508efae1c
Brian Olore <brian@olore.net>
2020-01-21 17:04:48 -0500

    refactor out array functions to their own file

---
+++ source/commands/array.js
@@ -0,0 +1,54 @@
+
+const handleArrayIndexing = (jsonObject, filter) => {
+  if (filter === '[]') {
+    return jsonObject.map(obj => JSON.stringify(obj)).join("\n");
+  }
+
+  let innards = filter.match(/\[(.*?)\]/)[1];
+  if (innards.includes(':')) { // it's a range
+    if (/^:/.test(innards)) { // only end
+      let digit = Number(innards.match(/^:(-*\d)/)[1]);
+      if (digit < 0) {
+        return jsonObject.reverse().slice(0, digit);
+      } else {
+        return jsonObject.slice(0, digit);
+      }
+
+    } else if (/:$/.test(innards)) { // only start
+      let digit = Number(innards.match(/(-*\d):$/)[1]);
+      if (digit < 0) {
+        return jsonObject.reverse().slice(digit);
+      } else {
+        return jsonObject.slice(digit);
+      }
+
+    } else {
+      // TODO: not handling negatives here yet
+      let res = innards.match(/(\d):(\d)$/);
+      let start = Number(res[1]);
+      let end = Number(res[2]);
+      return jsonObject.slice(start, end);
+
+    }
+  } else {
+    if (/^\[-/.test(filter)) { // negative index
+      let digit = filter.match(/^\[-(\d)\]/)[1];
+      return jsonObject.reverse()[digit];
+    }
+  }
+
+  // fall back - shouldn't get here
+  return jsonObject;
+}
+
+const isArrayIndexing = (filter) => {
+  return ((filter.startsWith('[')
+      && !filter.startsWith('["')   // .["test"] (not array indexing)
+      && (!/\[\d+]/.test(filter)))  // .[1] (not array indexing)
+      || filter.includes(':'));     // .[1:4] (is array indexing)
+}
+
+module.exports = {
+  isArrayIndexing,
+  handleArrayIndexing
+};
--- source/jqx.js
+++ source/jqx.js
@@ -1,5 +1,6 @@
 const get = require('lodash.get');
 const handleSelect = require('./commands/select');
+const { handleArrayIndexing, isArrayIndexing } = require('./commands/array');
 
 const jqx = (jsonObject, filter) => {
   if (filter === '.') {
@@ -10,12 +11,6 @@ const jqx = (jsonObject, filter) => {
   return parse(jsonObject, filters);
 };
 
-const isArrayIndexing = (filter) => {
-  return ((filter.startsWith('[')
-      && !filter.startsWith('["')   // .["test"] (not array indexing)
-      && (!/\[\d+]/.test(filter)))  // .[1] (not array indexing)
-      || filter.includes(':'));     // .[1:4] (is array indexing)
-}
 
 const parse = (jsonObject, filters) => {
   if (filters && filters.length === 0) {
@@ -31,7 +26,7 @@ const parse = (jsonObject, filters) => {
       return parse(handleArrayIndexing(jsonObject, filter), filters);
     }
 
-    // just use plain lodash.get
+    // use our old friend lodash.get
     return parse(get(jsonObject, filter), filters);
 
   } else {
@@ -40,51 +35,10 @@ const parse = (jsonObject, filters) => {
       let res = handleSelect(jsonObject, filter, filters);
       return parse(res.jsonObject, res.filters);
     }
+
     console.log('returning undefined! because ', filter);
     return undefined; // when filter doesn't start with .
   }
-}
-
-const handleArrayIndexing = (jsonObject, filter) => {
-  if (filter === '[]') {
-    return jsonObject.map(obj => JSON.stringify(obj)).join("\n");
-  }
-
-  let innards = filter.match(/\[(.*?)\]/)[1];
-  if (innards.includes(':')) { // it's a range
-    if (/^:/.test(innards)) { // only end
-      let digit = Number(innards.match(/^:(-*\d)/)[1]);
-      if (digit < 0) {
-        return jsonObject.reverse().slice(0, digit);
-      } else {
-        return jsonObject.slice(0, digit);
-      }
-
-    } else if (/:$/.test(innards)) { // only start
-      let digit = Number(innards.match(/(-*\d):$/)[1]);
-      if (digit < 0) {
-        return jsonObject.reverse().slice(digit);
-      } else {
-        return jsonObject.slice(digit);
-      }
-
-    } else {
-      // TODO: not handling negatives here yet
-      let res = innards.match(/(\d):(\d)$/);
-      let start = Number(res[1]);
-      let end = Number(res[2]);
-      return jsonObject.slice(start, end);
-
-    }
-  } else {
-    if (/^\[-/.test(filter)) { // negative index
-      let digit = filter.match(/^\[-(\d)\]/)[1];
-      return jsonObject.reverse()[digit];
-    }
-  }
-
-  // fall back - shouldn't get here
-  return jsonObject;
 }
 
 module.exports = jqx;
